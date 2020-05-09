20fb1d59100df47e9fa627facc781f92755508a8
Adesh Khanna <adeshkhanna271@gmail.com>
2020-05-09 20:11:27 +0530

    [added] signOut and isAuthenticated method

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -4,6 +4,7 @@ import axios from '../axios-instance';
 class AuthStore {
     @observable email = '';
     @observable password = '';
+    @observable auth = false;
 
     @action signIn = () => {
 
@@ -15,10 +16,32 @@ class AuthStore {
         axios.post('/login', payload)
             .then(res => {
                 localStorage.setItem('token', res.data.token);
+                this.auth = true;
             }).catch(res => {
                 console.log(res.data);
         });
-    }
+    };
+
+    @action signOut = () => {
+
+        localStorage.removeItem('token');
+
+        axios.post('/logout')
+            .then(res => {
+                console.log('logout success');
+            }).catch(res => {
+                console.log(res.data);
+        });
+    };
+
+    @action isAuthenticated = () => {
+
+        if (localStorage.getItem('token')) {
+            return JSON.parse(localStorage.getItem('token'));
+        } else {
+            return false;
+        }
+    };
 }
 
 const store = new AuthStore();
