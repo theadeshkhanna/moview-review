085fee3df1b24fa26df7ba8dc723ce4d75d6f61e
Adesh Khanna <adeshkhanna271@gmail.com>
2020-05-25 21:14:19 +0530

    [added] style for landing page

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'http://d80aed32.ngrok.io/api'
+   baseURL: 'http://26f5bdb9.ngrok.io/api'
 });
 
 export default instance;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './Landing.css';
+import {Link} from "react-router-dom";
 
 const Landing = () => {
     return (
         <div className={classes.Landing}>
             <div className={classes.Head}>
-                <h1>What movies you want to search ?</h1>
+                <h2>Welcome to MovieReview, </h2>
+                <br />
+                <h1>Search and view reviews of 10,000+ movies.</h1>
+                <br />
+                <h3>SignUp for free and be started..</h3>
             </div>
+            <Link to="/sign-up" className={classes.LandButton}>Sign Up</Link>
         </div>
     );
 };
